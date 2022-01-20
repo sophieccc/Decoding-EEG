038e43b3c7a868e79a27feda80c0cd4d6e862a0a
Sophie Crowley <crowleso@tcd.ie>
2022-01-20 05:10:00 +0000

    [WIP] Reduce and Restore Filters
    We use the Greenwood equation to reduce the number of filters in the spectrogram and aperiodicity from 1025 to 16 in reduceFilterNumber.m
    
    restoreOriginalFilters should create 1025 bands from the 16-band versions of the spectrogram and aperiodicity. These functions are drafts and have not yet been tested.

---
+++ matlab/reduceFilterNumber.m
@@ -0,0 +1,41 @@
+function reduceFilterNumber(filenames, numFilters, featureIdx, fs)
+
+[~,c] = size(filenames);
+if (c > 1)
+    stim = combineStimFiles(filenames);
+    stimData = stim.data;
+else
+    fileData = load(filenames);
+    stim = fileData.stim;
+    stimData = stim.data;
+end
+
+[~, numObs] = size(stimData);
+feature = stimData{featureIdx,1};
+[numSamples, origFilters] = size(feature);
+newMat = zeros(numSamples,numFilters);
+maxFreq = floor(fs / 2);
+stepSize = maxFreq / origFilters;
+[lower,~,upper]= greenwud(numFilters, 1, maxFreq, 0);
+
+for obs = 1:numObs
+    obsData = stimData{featureIdx,obs};
+    [numSamples, origFilters] = size(obsData);
+    for i = 1:numSamples
+        sampleData = obsData(i,:);
+        for j = 1:numFilters
+            bottomFilt = floor(lower(j) / stepSize)+1;
+            topFilt = floor(upper(j) / stepSize)+1;
+            if topFilt > origFilters
+                topFilt = origFilters;
+            end
+            vals = sampleData(bottomFilt:topFilt);
+            totalFreq = sum(vals);
+            newMat(i,j) = totalFreq / ((topFilt - bottomFilt)+1);
+        end
+    end
+    obsData = newMat;
+    stimData{featureIdx,obs} = obsData;
+end
+stim.data = stimData;
+save("dataStim_16_2.mat","stim", "-v7.3");
